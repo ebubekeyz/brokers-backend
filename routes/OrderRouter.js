24ce833bb85aea51ef032d4076d627bac91291d7
ebubekeyz <ebubeofforjoe@gmail.com>
2025-08-14 14:11:27 +0100

    new

--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getOrderById,, deleteOrder, createOrder, getUserOrders } = require('../controllers/OrderController');
+const { getOrderById, deleteOrder, createOrder, getUserOrders } = require('../controllers/OrderController');
 const auth = require('../middleware/authentication.js');
 
 
 router.post("/", auth, createOrder);
+
 router.get("/", auth, getUserOrders);
 router.get("/:id", auth, getOrderById);
 router.delete("/:id", auth, deleteOrder);
